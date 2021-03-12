ea550981700332946eba919894b4110f6499fbc4
LeonDante-ctrl <maxgamerdu@gmail.com>
2021-03-12 09:04:36 +0300

    took it into deployment mode

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from flask_dbshell import DbShell
 from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager, Shell
+from flask_script import Manager, Shell, Server
 
 from pitches.app import db, create_app
 from pitches.models import User, Role, Permission, Topic, TopicGroup, Comment, PollVote, PollAnswer, Message
@@ -15,6 +15,7 @@ def make_shell_context():
                 Comment=Comment, PollVote=PollVote, PollAnswer=PollAnswer, Message=Message)
 
 
+manager.add_command('server', Server)
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
@@ -33,6 +34,26 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.command
+def insert_initial_data():
+    """Adds initial data to database."""
+    Role.insert_roles()
+    TopicGroup.insert_root_topic_group()
+
+
+@manager.command
+def insert_fake_data():
+    """Adds fake data to database."""
+    from utils import data_generator
+    data_generator.generate_fake_users()
+    data_generator.generate_fake_topic_groups()
+    data_generator.generate_fake_topics()
+    data_generator.generate_fake_comments()
+    data_generator.generate_fake_messages()
+    data_generator.generate_fake_polls()
+    data_generator.generate_fake_votes()
 
 
 if __name__ == '__main__':
